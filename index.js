7a639afd03d488ea1a72c13000d58e67819c1e77
Lewis Horwood <horwood.lewis@gmail.com>
2016-05-18 23:28:15 -0600

    Quick and dirty view done, app works

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ app.get('/', function(req, res){
     os = os.slice(os.indexOf('(') + 1, os.indexOf(';'));
     console.log(os);
 
-    res.end("");
+    res.end("<h1>Request Header Parser Microservice</h1>" +
+    "<p><em>Your IP:</em> " + ip + "</p>" +
+    "<p><em>Your browser language(s):</em> " + languages + "</p>" +
+    "<p><em>Your OS:</em> " + os + "</p>");
 })
 
 app.listen(process.env.PORT || 8080);
